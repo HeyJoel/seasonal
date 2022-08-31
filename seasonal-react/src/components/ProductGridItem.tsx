a777ee69bfb90e2de07337b9c1ed8aeabec0e0a8
Joel Mitchell <2780344+HeyJoel@users.noreply.github.com>
2022-08-31 18:17:22 +0100

    Implemented grid item icons

--- seasonal-react/src/components/ProductGridItem.tsx
+++ seasonal-react/src/components/ProductGridItem.tsx
@@ -4,6 +4,10 @@ import ProductSearchCriteria from "data/models/product-search-criteria";
 import ProductAvailabilityMatrix from './ProductAvailabilityMatrix';
 import { FiLink } from "@react-icons/all-files/fi/FiLink";
 import { BiRuler } from "@react-icons/all-files/bi/BiRuler";
+import { FaMedal } from "@react-icons/all-files/fa/FaMedal";
+import { IoIosTimer } from "@react-icons/all-files/io/IoIosTimer";
+import { MdFiberNew } from "@react-icons/all-files/md/MdFiberNew";
+import { HiLightningBolt } from "@react-icons/all-files/hi/HiLightningBolt";
 
 interface ProductGridItemProps {
     product: Product,
@@ -20,36 +24,36 @@ export default function ProductGridItem({ product, searchCriteria }: ProductGrid
     return (
         <div className="p-4 mb-5 border-2 border-green-700 hover:bg-yellow-50 rounded" onClick={navigateToProduct}>
 
-            <div className="flex">
-                <h3 className="text-3xl">{product.name}</h3>
+            <div className="flex items-center">
+                <h3 className="text-3xl mt-0">{product.name}</h3>
                 <span className="grow text-right">
                     { product.isBest(searchCriteria.month) && 
-                        <span 
-                            className="mat-icon star" 
+                        <FaMedal 
+                            className="text-2xl ml-2"
                             aria-hidden="false" 
                             aria-label="This item is at its best this time of year."
-                            title="This item is at its best this time of year.">B</span>
+                            title="This item is at its best this time of year." />
                     }
                     { product.hasShortSeason() && 
-                        <span 
-                            className="mat-icon bolt" 
+                        <HiLightningBolt 
+                            className="text-2xl ml-2"
                             aria-hidden="false" 
                             aria-label="This item is not usually available, get it while you can."
-                            title="This item is not usually available, get it while you can.">S</span>
+                            title="This item is not usually available, get it while you can." />
                     }
                     { product.isStartOfSeason(searchCriteria.month) && 
-                        <span 
-                            className="mat-icon auto_awesome" 
+                        <MdFiberNew 
+                            className="text-2xl ml-2"
                             aria-hidden="false" 
                             aria-label="This item has just become in season."
-                            title="This item has just become in season.">S</span>
+                            title="This item has just become in season." />
                     }
                     { product.isEndOfSeason(searchCriteria.month) && 
-                        <span 
-                            className="mat-icon schedule" 
+                        <IoIosTimer 
+                            className="text-2xl ml-2"
                             aria-hidden="false" 
                             aria-label="This item is at the end of it's season."
-                            title="This item is at the end of it's season.">E</span>
+                            title="This item is at the end of it's season." />
                     }
                 </span>
             </div>
